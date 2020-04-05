8b2dfbeeab060d765d7f01991593c1b52590e478
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-04-05 16:04:34 +0200

    fizzbuzz cpp tdd

---
+++ fizzbuzz/cpp/Code/FizzBuzzGenerator.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "FizzBuzzNumber.h"
+
+using namespace std;
+
+class FizzBuzzListGenerator {
+
+public:
+	FizzBuzzListGenerator();
+	~FizzBuzzListGenerator();
+
+	string generateListWithThisManyInts(const int numberOfInts) const;
+};
---
+++ fizzbuzz/cpp/Code/FizzBuzzNumber.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class FizzBuzzNumber
+{
+public:
+	FizzBuzzNumber(int number);
+	~FizzBuzzNumber();
+
+	bool isFizz() const;
+	bool isBuzz() const;
+
+	string getFizzBuzz();
+
+private:
+	int theNumber;
+	string fizzBuzzString;
+};

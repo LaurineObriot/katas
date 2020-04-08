90e14c51a47a77ec09386598e3bba57022d39bb9
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-04-08 10:26:56 +0200

    diamond haskell good

---
+++ diamond/Haskell/Diamond.hs
@@ -0,0 +1,13 @@
+module Diamond
+where
+import Data.List (inits)
+
+diamond :: Char -> [String]
+diamond l = mirror (map mirror diagonal)
+    where
+    diagonal = (reverse spaces) <+> (letters <:> spaces)
+    letters  = ['A'..l]
+    spaces   = take (length letters) (inits (repeat ' '))
+    (<+>)    = zipWith (++)
+    (<:>)    = zipWith (:)
+    mirror l = l ++ (tail (reverse l))
---
+++ diamond/Haskell/Main.hs
@@ -0,0 +1,5 @@
+import System.Environment (getArgs)
+import Diamond (diamond)
+
+main = getArgs >>=
+    putStr . unlines . diamond . head . head

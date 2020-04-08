3a25f7b87b79a67ed2841ebcf93f62a512316817
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-04-08 10:31:16 +0200

    ok

---
+++ diamond/C/diamond.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  char max = argv[1][0];
+  int size = max - 'A';
+  int col,row;
+  for(row = 0; row<size*2+1; row++) {
+	  for(col = 0; col<size*2+1; col++) {
+		  int letter = row < size ? row : size*2 - row;
+		  putchar(col-size == letter || size-col == letter ? 'A'+letter : ' ');
+	  }
+	  putchar('\n');
+  }
+}
